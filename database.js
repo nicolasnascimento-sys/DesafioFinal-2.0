33b7b3516eeb8c73cccd4251f089eb1365a5889d
nicolasnascimento-sys <nicolasnascimentorodrigues26@gmail.com>
2026-04-18 14:17:09 -0300

    Escrever + /- o que atualizou do seu projeto

--- database.js
+++ database.js
@@ -16,24 +16,33 @@ await db.exec(`
   voluntarios TEXT,          ---1 até 500
   veiculos TEXT,             ---1 até 300
   subrimendos TEXT           ---Faltando,Pouco,ok ainda,Muito,
-  stats_resolucao TEXT DEFAULT 'abrigo cheinho'
-  )
- `)
+  stats_resolucao TEXT DEFAULT 'abrigo disponível')
+  `)
  
 console.log("Banco de dados correto!!!")
 
-await db.exec(`
-    INSERT INTO banco(abrigos,localizacao,abrigados,voluntarios,veiculos,subrimendos)
+const checagem = await db.get(`SELECT COUNT (*)AS total FROM banco`)
+if(checagem.total===0){await db.exec(`
+    INSERT INTO banco(abrigos,localizacao,abrigados,voluntarios,veiculos,subrimendos,)
     VALUES('N','Norte','900','500','200','ok ainda'),
     ('S','Sul','1000','500','300','muito'),
     ('L','Leste','500','100','50','pouco'),
     ('O','Oeste','750','200','20','faltando')
     `);
-      
+
+}else{
+ console.log(`banco pronto com ${checagem.total}de banco`)
+}
 
 const todoOBanco = await db.all('SELECT * FROM banco')
 console.table(todoOBanco)
 
+
+
+
+
+return db;
 };
 
-criarBanco();
+module.exports = {criarBanco}
+
