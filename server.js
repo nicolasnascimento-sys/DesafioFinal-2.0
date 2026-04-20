157f9a7d940df693e8499e027d68d39e3364335b
nicolasnascimento-sys <nicolasnascimentorodrigues26@gmail.com>
2026-04-20 18:33:31 -0300

    Escrever + /- o que atualizou do seu projeto

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require ('express')
+const {criarBanco} = require('./database')
+
+const app = express()
+
+app.use(express.json())
+
+ app.get('/',(req, res)=> {
+    res.send(`
+        <body>
+       <h1> Abrigos </h1>
+       <h2>Gestão de Abrigos<h2>
+       <p> </p>
+        </body>
+        `)
+ })
+ const PORT = 3000
+ app.listen(PORT, ()=> {
+    console.log(`servidor rodando em http://localhost:${PORT}`)
+
+ })
+app.get("/banco", async (req,res) => {const db = await criarBanco() 
+
+const obanco = await db.all(`SELECT * FROM banco`)
+
+res.json(obanco)
+
+})
+
+
+app.get("/banco/:id",async (req, res) =>{
+
+   const { id } = req.params
+
+   const db = await criarBanco()
+
+const bancoEspecifico = await db.all(`SELECT * FROM banco WHERE id = ?`,[id])
+
+
+res.json(bancoEspecifico)
+})
+
+
